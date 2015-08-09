db1c10bf0f616702332d5b48d2b3d53ab34e974c
ohnozzy <ohnozzy@gmail.com>
2015-08-09 12:34:43 +0800

    Initial Upload

---
+++ plot1.R
@@ -0,0 +1,6 @@
+plot1<-function(file="household_power_consumption.txt"){
+  var_names=c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+  var_class=c("character", "character", "numeric","numeric","numeric","numeric","numeric","numeric","numeric")
+  data<-read.csv2(file,skip=66636, nrows=69517-66637, na.strings = "?",  dec = ".",col.names = var_names, colClasses=var_class)
+  hist(data$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", col="red")
+}
